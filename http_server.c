9b35b2d1a2939dd4aff25261d53a29367dba50c9
Charlie <charles.caron@tufts.edu>
2020-04-18 18:00:09 -0400

    Server side file

---
+++ http_server.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <openssl/pem.h>
+#include <openssl/x509.h>
+#include <openssl/evp.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+EVP_PKEY * generate_key();
+X509 * generate_x509(EVP_PKEY * pkey, char * CN);
+
+EVP_PKEY * generate_key() {
+    /* Allocate memory for the EVP_PKEY structure. */
+    EVP_PKEY * pkey = EVP_PKEY_new();
+    if(!pkey)
+    {
+        return NULL;
+    }
+    
+    /* Generate the RSA key and assign it to pkey. */
+    FILE *fp = fopen("./fakeCA.key", "r");
+    RSA * rsa = PEM_read_RSAPrivateKey(fp, NULL, NULL, "elephantmen");
+    if(!EVP_PKEY_assign_RSA(pkey, rsa))
+    {
+        EVP_PKEY_free(pkey);
+        return NULL;
+    }
+    
+    /* The key has been generated, return it. */
+    return pkey;
+}
+
+/* Generates a self-signed x509 certificate. */
+X509 * generate_x509(EVP_PKEY * pkey, char * CN) {
+    /* Allocate memory for the X509 structure. */
+    X509 * x509 = X509_new();
+    if(!x509)
+    {
+        return NULL;
+    }
+    
+    /* Set the serial number. */
+    ASN1_INTEGER_set(X509_get_serialNumber(x509), 1);
+    
+    /* This certificate is valid from now until exactly one year from now. */
+    X509_gmtime_adj(X509_get_notBefore(x509), 0);
+    X509_gmtime_adj(X509_get_notAfter(x509), 31536000L);
+    
+    /* Set the public key for our certificate. */
+    X509_set_pubkey(x509, pkey);
+    
+    /* We want to copy the subject name to the issuer name. */
+    X509_NAME * name = X509_get_subject_name(x509);
+    
+    /* Set the country code and common name. */
+    X509_NAME_add_entry_by_txt(name, "C",  MBSTRING_ASC, (unsigned char *)"CA",        -1, -1, 0);
+    X509_NAME_add_entry_by_txt(name, "O",  MBSTRING_ASC, (unsigned char *)"MyCompany", -1, -1, 0);
+    X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (unsigned char *) CN, -1, -1, 0);
+    
+    /* Now set the issuer name. */
+    X509_set_issuer_name(x509, name);
+    
+    /* Actually sign the certificate with our key. */
+    if(!X509_sign(x509, pkey, EVP_sha1()))
+    {
+        printf("here\n");
+        X509_free(x509);
+        return NULL;
+    }
+    
+    return x509;
+}
+
+int create_socket(int port)
+{
+    int s;
+    struct sockaddr_in addr;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+    perror("Unable to create socket");
+    exit(EXIT_FAILURE);
+    }
+
+    if (bind(s, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    perror("Unable to bind");
+    exit(EXIT_FAILURE);
+    }
+
+    if (listen(s, 1) < 0) {
+    perror("Unable to listen");
+    exit(EXIT_FAILURE);
+    }
+
+    return s;
+}
+
+void init_openssl()
+{ 
+    SSL_load_error_strings();   
+    OpenSSL_add_ssl_algorithms();
+    OpenSSL_add_all_ciphers();
+}
+
+void cleanup_openssl()
+{
+    EVP_cleanup();
+}
+
+SSL_CTX *create_context()
+{
+    const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    method = SSLv23_server_method();
+
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+    perror("Unable to create SSL context");
+    ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_context(SSL_CTX *ctx, X509 * cert)
+{
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+    SSL_CTX_set_cipher_list(ctx, "ALL");
+
+    /* Set the key and cert */
+    if (SSL_CTX_use_certificate(ctx, cert) <= 0) {
+        ERR_print_errors_fp(stderr);
+    exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int sock;
+    SSL_CTX *ctx;
+
+    init_openssl();
+    ctx = create_context();
+
+    EVP_PKEY * pkey = generate_key();
+    X509 * cert = generate_x509(pkey, "google.com");
+    configure_context(ctx, cert);
+
+    sock = create_socket(9000);
+    int optval = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, 
+         (const void *)&optval , sizeof(int));
+
+    /* Handle connections */
+    while(1) {
+        struct sockaddr_in addr;
+        uint len = sizeof(addr);
+        SSL *ssl;
+        const char reply[] = "test\n";
+
+        int client = accept(sock, (struct sockaddr*)&addr, &len);
+        if (client < 0) {
+            perror("Unable to accept");
+            exit(EXIT_FAILURE);
+        }
+
+        ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client);
+
+        if (SSL_accept(ssl) <= 0) {
+            STACK_OF(SSL_CIPHER) *ciphers = SSL_get_ciphers(ssl);
+            printf("%s\n", (char*) ciphers);
+            printf("%d\n", strlen((char*)ciphers));
+            ERR_print_errors_fp(stderr);
+        }
+        else {
+            SSL_write(ssl, reply, strlen(reply));
+        }
+
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(client);
+    }
+
+    close(sock);
+    SSL_CTX_free(ctx);
+    cleanup_openssl();
+}
